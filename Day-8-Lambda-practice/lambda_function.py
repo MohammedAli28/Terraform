9f052b624a7112e5e86647ba1170f7a727fea784
Mohammed Mumtaz Ahmed Ali <mdmumtazali28@gmail.com>
2026-04-07 00:07:27 +0530

    added files

--- Day-8-Lambda-practice/lambda_function.py
+++ Day-8-Lambda-practice/lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Ali for s3!')
+        'body': json.dumps('Hello from Ali for event bridge!')
     }
